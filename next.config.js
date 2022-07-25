17e2cc6282c4b32f95d8f5477d1d7a67e8e760cc
d3b0nair <60494717+d3b0nair@users.noreply.github.com>
2022-07-24 19:58:23 -0500

    setup eslint for typescript, add custom stuff to tailwind, includ svg in next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
   },
   images: { domains: ['cdn.shopify.com'] },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: `@media (min-width: 480px) { ... }`,
+      },
+      colors: {
+        background: '#faf5f1',
+        primary: '#f59e0b',
+        secondary: '#92400e',
+        white: '#f8fafc',
+        darkBrown: '#61423c',
+        offGrey: '#cfcac6',
+        grey: '#6b7280',
+        darkestGrey: '#111827',
+      },
+    },
   },
   plugins: [],
 };
