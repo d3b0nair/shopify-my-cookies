87dbaac4f9c505fdf7fb0c9684bcff76cc39880b
d3b0nair <60494717+d3b0nair@users.noreply.github.com>
2022-08-03 13:01:14 -0500

    add useRipple hook, refactor helpers

---
+++ hooks/useRipple.tsx
@@ -0,0 +1,54 @@
+import { useEffect, MouseEvent, useState, useMemo } from 'react';
+
+export default function useRipple(duration: number, customSize?: number) {
+  const [rippleArray, setRippleArray] = useState<Array<IRipple>>([]);
+
+  const ripples = useMemo(() => rippleArray.map((item) => item), [rippleArray]);
+
+  const useDebouncedRippleCleanUp = (
+    rippleCount: number,
+    duration: number,
+    cleanUpFunction: () => void
+  ) => {
+    useEffect(() => {
+      let bounce: ReturnType<typeof setTimeout>;
+      if (rippleCount > 0) {
+        bounce = setTimeout(() => {
+          cleanUpFunction();
+          clearTimeout(bounce);
+        }, duration);
+      }
+
+      return () => clearTimeout(bounce);
+    }, [rippleCount, duration, cleanUpFunction]);
+  };
+
+  useDebouncedRippleCleanUp(ripples.length, duration, () => {
+    setRippleArray([]);
+  });
+
+  const createRipple = (evt: MouseEvent) => {
+    const { left, top } = evt.currentTarget.getBoundingClientRect();
+    const size = !customSize
+      ? Math.min(evt.currentTarget.clientHeight, evt.currentTarget.clientWidth)
+      : customSize;
+
+    const x = evt.clientX - left - size / 2;
+    const y = evt.clientY - top - size / 2;
+    const newRipple = {
+      x,
+      y,
+      size,
+    };
+
+    setRippleArray([...rippleArray, newRipple]);
+  };
+
+  return [createRipple, ripples] as const;
+}
+
+interface IRipple {
+  x: number;
+  y: number;
+  size: number;
+}
---
+++ utils/heroList.ts
@@ -0,0 +1,6 @@
+import heroImage0 from '../public/hero.png';
+import heroImage1 from '../public/hero1.png';
+import heroImage2 from '../public/hero2.png';
+import heroImage3 from '../public/hero3.png';
+
+export const heroImages = [heroImage0, heroImage1, heroImage2, heroImage3];
